d1e30c49758c1bb73aef9a2536b8f9b9f82a91b6
tky3a <takayamac@icloud.com>
2018-02-24 13:52:14 +0900

    Install the devise

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'pry-byebug'
 gem 'pry-rails'
   #Bootstrap
   gem 'bootstrap-sass', '3.3.1'
+  gem 'devise'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,20 @@ module ApplicationHelper
            "#{page_title}"
       end
   end
+
+#flashメッセージの装飾に命令を追加
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-danger"
+      when :alert
+        "alert-warning"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
