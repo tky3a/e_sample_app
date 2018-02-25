d90f693cea0f456feb4a3c1b9371424c8d5c5da9
tky3a <takayamac@icloud.com>
2018-02-25 11:08:21 +0900

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#home' 
+  resources :users, only: [:show]
+  root 'static_pages#home'
 
   match '/help', to: 'static_pages#help', via: 'get'
 
